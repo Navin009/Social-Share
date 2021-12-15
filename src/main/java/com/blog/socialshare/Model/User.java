6fbe3d87ae46f36cd5c4caad5de9dc0de81264ca
Navin Kumar <navin.kumar.18@gmail.com>
2021-12-15 23:43:23 +0530

    sprin jpa work with database

--- src/main/java/com/blog/socialshare/Model/User.java
+++ src/main/java/com/blog/socialshare/Model/User.java
@@ -1,6 +1,14 @@
 package com.blog.socialshare.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"user\"")
 public class User {
+
+    @Id
     private Integer id;
     private String name;
     private String email;
