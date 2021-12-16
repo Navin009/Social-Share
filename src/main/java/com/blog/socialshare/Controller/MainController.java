9cdcb276c02942bb3bfe92511bf8d411c26adb56
Navin Kumar <navin.kumar.18@gmail.com>
2021-12-16 18:20:11 +0530

    Create PostSearchDao

--- src/main/java/com/blog/socialshare/Controller/MainController.java
+++ src/main/java/com/blog/socialshare/Controller/MainController.java
@@ -1,14 +1,23 @@
 package com.blog.socialshare.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
-    @RequestMapping("")
+    @RequestMapping(path = "")
     public String index() {
+        return "index";
+    }
+
+    @GetMapping(params = "search")
+    public String searchedindex(@RequestParam(value = "search") String search, Model model) {
+        model.addAttribute("search", search);
         return "index";
     }
 }
---
+++ src/main/java/com/blog/socialshare/Dao/PostSearchDao.java
@@ -0,0 +1,11 @@
+package com.blog.socialshare.Dao;
+
+import java.util.List;
+
+import com.blog.socialshare.Model.Post;
+
+public class PostSearchDao {
+    public List<Post> searchPost(String query) {
+        return null;
+    }
+}
