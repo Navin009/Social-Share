470cb9aad1a0c4f8e51f19ed04d05b7042b42cbc
Navin Kumar <navin.kumar.18@gmail.com>
2022-01-02 19:18:48 +0530

    TagController Updated

--- src/main/java/com/blog/socialshare/controller/TagController.java
+++ src/main/java/com/blog/socialshare/controller/TagController.java
@@ -6,19 +6,17 @@ import com.blog.socialshare.model.Tag;
 import com.blog.socialshare.service.TagService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TagController {
 
     @Autowired
     private TagService tagService;
 
     @GetMapping(value = "tag/search")
-    @ResponseBody
     public List<Tag> searchTag(@RequestParam("tag") String tag) {
         return tagService.getTagsByName(tag);
     }
