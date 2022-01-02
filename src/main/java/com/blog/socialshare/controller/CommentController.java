27e395c1f10b8ac6b53fe563a9883741fa043e5c
Navin Kumar <navin.kumar.18@gmail.com>
2022-01-02 19:04:26 +0530

    Comment Controller Updated

--- src/main/java/com/blog/socialshare/controller/CommentController.java
+++ src/main/java/com/blog/socialshare/controller/CommentController.java
@@ -5,45 +5,42 @@ import javax.servlet.http.HttpServletRequest;
 import com.blog.socialshare.service.CommentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/")
+@RestController
 public class CommentController {
 
     @Autowired
     private CommentService commentService;
 
     @PostMapping("/comment/save")
-    public String saveComment(HttpServletRequest request) {
+    public ResponseEntity<String> saveComment(HttpServletRequest request) {
         System.out.println(request.getParameter("postid"));
         Integer postId = Integer.parseInt(request.getParameter("postid"));
         String comment = request.getParameter("comment");
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         commentService.saveComment(postId, name, email, comment);
-        return "redirect:/blog/" + postId;
+        return ResponseEntity.ok("Comment saved successfully");
     }
 
     @PutMapping(path = "updatecomment/{commentId}")
-    @ResponseBody
-    public String updateComment(@PathVariable("commentId") Integer commentId, @RequestBody String comment) {
+    public ResponseEntity<String> updateComment(@PathVariable("commentId") Integer commentId,
+            @RequestBody String comment) {
         commentService.updateComment(commentId, comment);
-        return "success";
+        return ResponseEntity.ok("Comment updated successfully");
     }
 
     @DeleteMapping(path = "deletecomment/{commentId}")
-    @ResponseBody
-    public String deleteComment(@PathVariable("commentId") Integer commentId) {
+    public ResponseEntity<String> deleteComment(@PathVariable("commentId") Integer commentId) {
         commentService.deleteComment(commentId);
-        return "success";
+        return ResponseEntity.ok("Comment deleted successfully");
     }
 
 }
