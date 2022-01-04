a4615233c7f2bd2e111872a5da084f74b5821be7
Navin Kumar <navin.kumar.18@gmail.com>
2022-01-04 15:32:49 +0530

    UI changes

--- src/main/java/com/blog/socialshare/repository/CommentRepository.java
+++ src/main/java/com/blog/socialshare/repository/CommentRepository.java
@@ -3,6 +3,8 @@ package com.blog.socialshare.repository;
 import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import com.blog.socialshare.model.Comment;
 import com.blog.socialshare.model.Post;
 
@@ -11,7 +13,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
--- src/main/resources/static/js/blog.js
+++ src/main/resources/static/js/blog.js
@@ -1,15 +1,15 @@
 function deletePost() {
-	if (confirm('Are you sure you want to delete this post?')) {
-		let postId = window.location.pathname.split('/')[2];
+	if (confirm("Are you sure you want to delete this post?")) {
+		let postId = window.location.pathname.split("/")[2];
 		let xhr = new XMLHttpRequest();
-		xhr.open('DELETE', '/blog/delete/' + postId);
-		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.open("DELETE", "/blog/delete/" + postId);
+		xhr.setRequestHeader("Content-Type", "application/json");
 		xhr.onload = function () {
 			if (xhr.status === 200) {
-				alert('Post deleted');
-				window.location.href = '/';
+				alert("Post deleted");
+				window.location.href = "/";
 			} else {
-				alert('An error occurred');
+				alert("An error occurred");
 			}
 		};
 		xhr.send();
@@ -17,62 +17,63 @@ function deletePost() {
 }
 
 function updatePost() {
-	if (confirm('Are you sure you want to update this post?')) {
-		let postId = window.location.pathname.split('/')[2];
-		window.location.href = '../updatepost/' + postId;
+	if (confirm("Are you sure you want to update this post?")) {
+		let postId = window.location.pathname.split("/")[2];
+		window.location.href = "../updatepost/" + postId;
 	}
 }
 
 function updateCommentToDatabase(commentId, commentElement, buttonElement) {
 	let commentData = commentElement.value;
 	let xhr = new XMLHttpRequest();
-	xhr.open('PUT', '../updatecomment/' + commentId);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.onload = function () {
-		if (xhr.status === 200) {
+	xhr.open("PUT", "../updatecomment/" + commentId);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) {
 			commentElement.innerHTML = commentData;
 			commentElement.readOnly = true;
-			commentElement.style.border = 'none';
-			buttonElement.innerHTML = 'Update';
+			commentElement.style.border = "none";
+			buttonElement.innerHTML = "Update";
 			buttonElement.onclick = () => updateComment(commentId, buttonElement);
-			alert('Comment updated');
+			alert("Comment updated");
 			window.location.reload();
-		} else {
-			alert('An error occurred');
+		}
+		if (xhr.status === 302) {
+			alert("Comment cannot be empty");
 		}
 	};
 	xhr.send(commentData);
 }
 
 function updateComment(commentId, buttonElement) {
-	if (confirm('Are you sure you want to update this comment?')) {
-		let commentElement = buttonElement.parentNode.parentNode.getElementsByTagName('textarea')[0];
+	if (confirm("Are you sure you want to update this comment?")) {
+		let commentElement = buttonElement.parentNode.parentNode.getElementsByTagName("textarea")[0];
 		commentElement.readOnly = false;
-		commentElement.style.border = '1px solid black';
-		buttonElement.innerHTML = 'Save Changes';
+		commentElement.style.border = "1px solid black";
+		buttonElement.innerHTML = "Save Changes";
 		buttonElement.onclick = () => updateCommentToDatabase(commentId, commentElement, buttonElement);
 	}
 }
 
 function deleteComment(commentId) {
-	if (confirm('Are you sure you want to delete this comment?')) {
+	if (confirm("Are you sure you want to delete this comment?")) {
 		let xhr = new XMLHttpRequest();
-		xhr.open('DELETE', '../deletecomment/' + commentId);
-		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.open("DELETE", "../deletecomment/" + commentId);
+		xhr.setRequestHeader("Content-Type", "application/json");
 		xhr.onload = function () {
 			if (xhr.status === 200) {
-				alert('Comment deleted');
+				alert("Comment deleted");
 				window.location.reload();
 			} else {
-				alert('An error occurred');
+				alert("An error occurred");
 			}
 		};
 		xhr.send();
 	}
 }
 
-let commentDiv = document.getElementsByClassName('comments')[0].firstElementChild;
+let commentDiv = document.getElementsByClassName("comments")[0].firstElementChild;
 
 if (commentDiv == null) {
-	document.getElementsByClassName('comments')[0].style.display = 'none';
+	document.getElementsByClassName("comments")[0].style.display = "none";
 }
